fa6e8746a508e8956cf1a7b25c6a5b85b2659573
tongxiong <tongxiong.9901@gmail.com>
2024-09-04 21:11:42 -0700

    menuItemsClass

--- MenuItem.h
+++ MenuItem.h
@@ -1,13 +1,23 @@
-//
-// Created by Admin on 9/4/2024.
-//
+#include <iostream>
+#include <string>
 
 #ifndef FOODDASH_MENUITEM_H
 #define FOODDASH_MENUITEM_H
 
 
 class MenuItem {
+private:
+    std::string itemName;
+    float itemPrice;
 
+    MenuItem(std::string name, float price) {
+        itemName = name;
+        itemPrice = price;
+    };
+
+public:
+    std::string getItemName() {}
+    float getItemPrice() { return itemPrice; }
 };
 
 
