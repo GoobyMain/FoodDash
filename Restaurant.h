b99d8d19e2c6b3a28be95a12ffeb137e74268ae5
Tong Xiong <121007584+TongXiong@users.noreply.github.com>
2024-09-11 19:34:49 +0000

    RestaurantClassincomplete

--- Restaurant.h
+++ Restaurant.h
@@ -2,12 +2,24 @@
 // Created by Admin on 9/4/2024.
 //
 
+#include "Order.h"
+<string>
+<vector>
+using std::vector, std::string;
 #ifndef FOODDASH_RESTAURANT_H
 #define FOODDASH_RESTAURANT_H
 
 
 class Restaurant {
+    private:
+    string name;
+    vector<string> menuItem;
+    vector<nextOrderNumber> orderNumList;
 
+    public:
+    Restaurant(string stand);
+
+    getName();
 };
 
 
